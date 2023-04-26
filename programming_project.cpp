b0a971284677a51cc8e8c85e09ed0604b1d4bc15
1biue <112619398+1biue@users.noreply.github.com>
2023-04-26 21:37:53 +0800

    Update programming_project.cpp

--- programming_project.cpp
+++ programming_project.cpp
@@ -1,7 +1,9 @@
 	#include <iostream>
-	#include <fstream>	// for file input/output
-	#include <string>
+	#include <fstream>
+	#include <sstream>
 	#include <vector>
+	#include <string>
+	#include <algorithm>
 
 	using namespace std;
 
