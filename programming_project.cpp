c63e489627900b957ad9a57d6baef8246b0c3bb4
1biue <112619398+1biue@users.noreply.github.com>
2023-04-26 18:38:16 +0800

    main rebuild

--- programming_project.cpp
+++ programming_project.cpp
@@ -2,251 +2,8 @@
 	#include <fstream>	// for file input/output
 	#include <string>
 	#include <vector>
-    #include <algorithm>
-    #include <sstream>
 
 	using namespace std;
-	
-	struct Book {
-		string id;
-		string title;
-		string author;
-		string publisher;
-		int year;
-		bool available;
-	};
-	bool compareTitle(const Book& book1, const Book& book2) {
-		return book1.title < book2.title;
-	}
-	void displayBooks(const vector<Book>& books) {
-		cout << setw(12) << "ID" << setw(50) << "Book Details" << setw(15) << "Availability" << endl;
-		for (const auto& book : books) {
-			cout << setw(12) << book.id << setw(50) << book.title + " by " + book.author + ", " + book.publisher + ", " + to_string(book.year) << setw(15) << (book.available ? "Available" : "Not available") << endl;
-		}
-	}
-    vector<Book> books = {
-			{"CH6717212", "10 Little Indians", "Agatha Christie", "Pocket", 1982, true},
-			{"NY8167261", "102 School Cafeteria Jokes", "Morgan Matthews", "Troll Communications Llc", 1992, true},
-			{"MJ1586212", "2nd Chance", "James Patterson", "Time Warner Audio Major", 2002, true},
-			{"MJ8118129", "52 Ways to Make a Difference", "Lynn Gordon", "Chronicle Books", 1996, true},
-		};
-
-		// Display books
-	int main() {displayBooks(books);
-		return 0;
-	} 
-	// Function to search for books based on keywords
-	vector<Book> search_books(vector<Book>& books, string keywords) {
-		vector<Book> result;
-
-		// Split the input keywords into individual keywords
-		vector<string> search_terms;
-		string term;
-		for (char c : keywords) {
-			if (c == ' ' && !term.empty()) {
-				search_terms.push_back(term);
-				term = "";
-			}
-			else if (c == '"' && term.empty()) {
-				// Phrase enclosed in double quotes
-				auto end_pos = keywords.find('"', 1);
-				if (end_pos == string::npos) {
-					// If there is no ending double quote, treat the whole input as a single keyword
-					term = keywords.substr(1);
-				}
-				else {
-					term = keywords.substr(1, end_pos - 1);
-				}
-				search_terms.push_back(term);
-				term = "";
-				keywords.erase(0, end_pos + 1);
-			}
-			else {
-				term += c;
-			}
-		}
-		if (!term.empty()) {
-			search_terms.push_back(term);
-		}
-
-		// Search for books that match the keywords
-		for (auto& book : books) {
-			bool is_match = false;
-			for (auto& term : search_terms) {
-				if (term.front() == '"' && term.back() == '"') {
-					// Exact phrase search
-					term = term.substr(1, term.length() - 2);
-					if (book.id == term || book.title == term || book.author == term || book.publisher == term) {
-						is_match = true;
-						break;
-					}
-				}
-				else {
-					// Case insensitive keyword search
-					transform(term.begin(), term.end(), term.begin(), ::tolower);
-					if (book.id.find(term) != string::npos || book.title.find(term) != string::npos || book.author.find(term) != string::npos || book.publisher.find(term) != string::npos) {
-						is_match = true;
-						break;
-					}
-				}
-			}
-			if (is_match) {
-				result.push_back(book);
-			}
-		}
-
-		return result;
-	}
-
-	vector<Book> bookList;
-
-	bool isIdUnique(string id) {
-		for (Book book : bookList) {
-			if (book.id == id) {
-				return false;
-			}
-		}
-		return true;
-	}
-
-	bool isYearPositive(int year) {
-		if (year <= 0) {
-			return false;
-		}
-		return true;
-	}
-
-	void addBook() {
-		Book book;
-		cout << "Enter ID (max 10 characters): ";
-		cin >> book.id;
-		if (!isIdUnique(book.id)) {
-			cout << "Error: ID already exists.\n";
-			return;
-		}
-		cout << "Enter title (max 100 characters): ";
-		cin.ignore();
-		getline(cin, book.title);
-		cout << "Enter author(s) (max 50 characters each, separated by ';'): ";
-		string authorsStr;
-		getline(cin, authorsStr);
-		string author;
-		size_t pos = 0;
-		while ((pos = authorsStr.find(";")) != string::npos) {
-			author = authorsStr.substr(0, pos);
-			if (author.length() > 50) {
-				cout << "Error: Author name is too long.\n";
-				return;
-			}
-			book.authors.push_back(author);
-			authorsStr.erase(0, pos + 1);
-		}
-		if (authorsStr.length() > 50) {
-			cout << "Error: Author name is too long.\n";
-			return;
-		}
-		book.authors.push_back(authorsStr);
-		cout << "Enter publisher (max 50 characters): ";
-		getline(cin, book.publisher);
-		cout << "Enter year: ";
-		cin >> book.year;
-		if (!isYearPositive(book.year)) {
-			cout << "Error: Year must be a positive integer.\n";
-			return;
-		}
-		book.available = true;
-		bookList.push_back(book);
-		cout << "Book added successfully!\n";
-	}
-
-	int main() {
-		addBook();
-		return 0;
-	}
-
-
-
-	int main() {
-		// Example data
-		vector<Book> books = {
-			{"001", "C++ Programming", "John Smith", "ABC Publishing"},
-			{"002", "Introduction to C++", "Jane Doe", "XYZ Press"},
-			{"003", "Japanese Language for Beginners", "Tomoko Yamada", "PQR Books"},
-			{"004", "C++ Language for Beginners", "Sam Lee", "XYZ Press"},
-			{"005", "Introduction to Python", "Alice Wong", "ABC Publishing"}
-		};
-
-		// Ask the user to input the search keywords
-		string keywords;
-		cout << "Enter one or more keywords separated by space, and with or without double quotes:" << endl;
-		getline(cin, keywords);
-
-		// Search for books based on the search keywords
-		auto result = search_books(books, keywords);
-
-		// Display the search results
-		cout << "Search results for \"" << keywords << "\":" << endl;
-		for (auto& book : result) {
-			cout << book.id << " | " << book.title << " | " << book.author << " | " << book.publisher << endl;
-		}
-
-
-	}
-	
-	Book* findBookById(string id) {
-		for (Book& book : bookList) {
-			if (book.id == id) {
-				return &book;
-			}
-		}
-		return nullptr;
-	}
-
-	void removeBook() {
-		string id;
-		cout << "Enter ID of book to remove: ";
-		cin >> id;
-		Book* bookPtr = findBookById(id);
-		if (bookPtr == nullptr) {
-			cout << "Error: Book not found.\n";
-			return;
-		}
-		Book book = *bookPtr;
-		if (!book.available) {
-			cout << "Error: Book is not available.\n";
-			return;
-		}
-		cout << "Book found!\n";
-		cout << "ID: " << book.id << endl;
-		cout << "Title: " << book.title << endl;
-		cout << "Author(s): ";
-		for (size_t i = 0; i < book.authors.size(); i++) {
-			cout << book.authors[i];
-			if (i != book.authors.size() - 1) {
-				cout << "; ";
-			}
-		}
-		cout << endl;
-		cout << "Publisher: " << book.publisher << endl;
-		cout << "Year: " << book.year << endl;
-		char choice;
-		cout << "Are you sure you want to delete this book? (Y/N): ";
-		cin >> choice;
-		if (choice == 'Y' || choice == 'y') {
-			bookList.erase(bookList.begin() + (bookPtr - &bookList[0]));
-			cout << "Book removed successfully!\n";
-		}
-		else {
-			cout << "No book removed.\n";
-		}
-	}
-
-	int main() {
-		// assume bookList has been initialized with some books
-		removeBook();
-		return 0;
-	}
-
 
 	int extractFields(string line, char fields[][101]) {
 		int numFields = 0;
@@ -562,6 +319,8 @@
 			"********************************* \n"
 			"Option(1 - 7) :";
 		cin >> mode;
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
 		while (true) {
 			switch (mode) {
@@ -587,16 +346,35 @@
 				//
 				break;
 			case 7:
-				exit(0);
+				char confirmExit;
+				do {
+					cout << "Are you sure you want to exit? (Y/N): ";
+					cin >> confirmExit;
+					confirmExit = tolower(confirmExit);
+					if (confirmExit == 'y') {
+						exit(0);
+					}
+					else if (confirmExit == 'n') {
+						cout << "********************************* \n"
+							"Option(1 - 7) :";
+						cin >> mode;
+						cin.clear();
+						cin.ignore(numeric_limits<streamsize>::max(), '\n');
+					}
+					else {
+						cout << "Invalid input. Please enter 'Y' or 'N'.\n";
+					}
+				} while (confirmExit != 'y' && confirmExit != 'n');
 				break;
 			default:
 				cout << "Enter number between 1-7 only \n"
 					"********************************* \n"
 					"Option(1 - 7) :";
 				cin >> mode;
+				cin.clear();
+				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			}
 		}
 		return 0;
 	}
-
 
